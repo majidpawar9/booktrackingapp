a201044560fc16cd3b6aae98bc5d7c9210b13340
majidpawar9 <majidpawar9@gmail.com>
2023-08-18 20:49:06 -0400

    added comments

--- Data/BlogContext.cs
+++ Data/BlogContext.cs
@@ -8,8 +8,10 @@ namespace booktrackingapp.Data
         public BlogContext(DbContextOptions<BlogContext> options) 
             : base(options)
         {
-
+            // This constructor is used to configure the DbContext with the provided options.
         }
+        // DbSet property to represent the "Books" table in the database.
+        // The "= null!" ensures that it's initialized as non-null, even if it's not set in the constructor.
         public DbSet<Book> Books { get; set; } =null!;
         public DbSet<Category> Categories { get; set; } =null!;
         public DbSet<CategoryType> CategoryTypes { get; set; } =null!;
